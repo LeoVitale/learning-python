486418c0e66b86aa53f81aa79e9ca421307bd645
Leonardo Vitale <leonardo.vitale@xelix.com>
2024-10-20 22:14:31 +0100

    feat: Refactor character classes and add attack/enemy types

--- combate-game/combate_game/main.py
+++ combate-game/combate_game/main.py
@@ -1,73 +1,80 @@
 from colorama import init, Fore, Style
 from random import randint
+from enum import Enum
 
 
 init(autoreset=True)
 
 
+class AttackType(Enum):
+    SUPER_ATTACK = "Super Attack"
+    MAGIC_ATTACK = "Magic Attack"
+
+
+class EnemyType(Enum):
+    FIRE = "Fire"
+    ICE = "Ice"
+    EARTH = "Earth"
+    WIND = "Wind"
+
+
 class Character:
-    def __init__(self, name: str, health: int, level: int):
-        self.__name = name
-        self.__health = health
-        self.level = level
+    def __init__(self, name: str, health: int, level: int) -> None:
+        self.name: str = name
+        self._health: int = health
+        self.level: int = level
 
-    def get_name(self):
-        return self.__name
+    @property
+    def health(self) -> int:
+        return self._health
 
-    def get_health(self):
-        return self.__health
+    @health.setter
+    def health(self, health: int) -> None:
+        self._health = max(health, 0)
 
-    def get_level(self):
-        return self.level
+    @property
+    def is_dead(self) -> bool:
+        return self._health <= 0
 
-    def show_stats(self):
-        return f"\nName: {self.get_name()}\nHealth: {self.get_health()}\nLevel: {self.get_level()}\n"
+    def show_stats(self) -> str:
+        return f"\nName: {self.name}\nHealth: {self.health}\nLevel: {self.level}\n"
 
-    def attack(self, enemy: "Character"):
-        damage = randint(self.get_level() * 2, self.get_level() * 4)
-        enemy.update_health(damage)
+    def attack(self, other: "Character") -> None:
+        damage: int = randint(self.level * 2, self.level * 4)
+        other.health -= damage
         print(
-            f"\n{Fore.YELLOW}{self.get_name()} attacked {enemy.get_name()} for {damage} damage{Style.RESET_ALL}\n"
+            f"\n{Fore.YELLOW}{self.name} attacked {other.name} for {damage} damage{Style.RESET_ALL}\n"
         )
 
-    def update_health(self, damage: int):
-        self.__health -= damage
-        if self.__health <= 0:
-            self.__health = 0
-            print(f"{Fore.RED}{self.get_name()} has been defeated{Style.RESET_ALL}")
 
-
-# Hero
 class Hero(Character):
-    def __init__(self, name: str, health: int, level: int, attack_type: str):
+    def __init__(
+        self, attack_type: AttackType, name: str, health: int, level: int
+    ) -> None:
         super().__init__(name, health, level)
-        self.__attack_type = attack_type
+        self.attack_type: AttackType = attack_type
 
-    def get_attack_type(self):
-        return self.__attack_type
+    def show_stats(self) -> str:
+        return f"{super().show_stats()}\nAttack Type: {self.attack_type.value}"
 
-    def show_stats(self):
-        return f"{super().show_stats()}\nAttack Type: {self.get_attack_type()}"
-
-    def magic_attack(self, enemy: "Character"):
-        damage = randint(self.get_level() * 5, self.get_level() * 8)
-        enemy.update_health(damage)
+    def magic_attack(self, enemy: "Character") -> None:
+        damage: int = randint(self.level * 5, self.level * 8)
+        enemy.health -= damage
         print(
-            f"\n{Fore.CYAN}{self.get_name()} used a magic attack on {enemy.get_name()} for {damage} damage{Style.RESET_ALL}\n"
+            f"\n{Fore.CYAN}{self.name} used a magic attack on {enemy.name} for {damage} damage{Style.RESET_ALL}\n"
         )
 
 
 # Enemy
 class Enemy(Character):
-    def __init__(self, name: str, health: int, level: int, enemy_type: str):
+    def __init__(
+        self, name: str, health: int, level: int, enemy_type: EnemyType
+    ) -> None:
         super().__init__(name, health, level)
-        self.__enemy_type = enemy_type
+        self.enemy_type: EnemyType = enemy_type
 
-    def get_enemy_type(self):
-        return self.__enemy_type
-
-    def show_stats(self):
-        return f"{super().show_stats()}\nEnemy Type: {self.get_enemy_type()}"
+    def show_stats(self) -> str:
+        return f"{super().show_stats()}\nEnemy Type: {self.enemy_type.value}"
 
 
 # Battle
@@ -76,15 +83,13 @@ class Battle:
     This class is responsible for managing the battle between the hero and the enemy.
     """
 
-    def __init__(self, hero: Hero, enemy: Enemy):
-        self.hero = hero
-        self.enemy = enemy
+    def __init__(self, hero: Hero, enemy: Enemy) -> None:
+        self.hero: Hero = hero
+        self.enemy: Enemy = enemy
 
-    def start_battle(self):
-        print(
-            f"Battle started between {self.hero.get_name()} and {self.enemy.get_name()}"
-        )
-        while self.hero.get_health() > 0 and self.enemy.get_health() > 0:
+    def start_battle(self) -> None:
+        print(f"Battle started between {self.hero.name} and {self.enemy.name}")
+        while not self.hero.is_dead and not self.enemy.is_dead:
             print(f"Hero details: {self.hero.show_stats()}")
             print(f"Enemy details: {self.enemy.show_stats()}")
 
@@ -100,23 +105,20 @@ class Battle:
             else:
                 print("Invalid attack type")
 
-            if self.enemy.get_health() > 0:
+            if not self.enemy.is_dead:
                 self.enemy.attack(self.hero)
 
-        if self.hero.get_health() > 0:
-            print(
-                f"\n{Fore.GREEN}{self.hero.get_name()} wins the battle{Style.RESET_ALL}"
-            )
-
+        if self.hero.is_dead:
+            print(f"\n{Fore.RED}{self.enemy.name} wins the battle{Style.RESET_ALL}")
         else:
-            print(
-                f"\n{Fore.RED}{self.enemy.get_name()} wins the battle{Style.RESET_ALL}"
-            )
+            print(f"\n{Fore.GREEN}{self.hero.name} wins the battle{Style.RESET_ALL}")
 
 
-hero = Hero(name="Arus", health=200, level=10, attack_type="Super Attack")
-enemy = Enemy(name="Mago", health=100, level=10, enemy_type="Fire")
+hero: Hero = Hero(
+    name="Arus", health=200, level=10, attack_type=AttackType.SUPER_ATTACK
+)
+enemy: Enemy = Enemy(name="Mago", health=100, level=10, enemy_type=EnemyType.FIRE)
 
 
-battle = Battle(hero, enemy)
+battle: Battle = Battle(hero, enemy)
 battle.start_battle()
